10d5e6397a558128776dd04a9a6cab3987740b5a
aAAa <oragaro4@gmail.com>
2020-05-28 19:41:22 +0700

    add intersection function

---
+++ src/intersection.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include "intersection.h"
+
+void intersection(float *X, float *Y, float *R, int n)
+{
+	int i,j;
+	float x,y,d;
+	for (i = 0; i < n; i++) 
+	{
+        for (j = i + 1; j < n; j++) 
+        {
+            x = X[i] - X[j];
+            y = Y[i] - Y[j];
+            d = sqrt(pow(x, 2) + pow(y, 2));
+            if (d <= (R[i] + R[j]))
+                printf("Окржности №%d и №%d пересекаются\n", i, j);
+        }
+    }
+}
